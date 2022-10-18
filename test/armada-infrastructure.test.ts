6dd4887ad624503c008796b4d5ce3f35150c019c
Sergio Pichardo <10542280+sergiopichardo@users.noreply.github.com>
2022-10-18 14:52:52 -0500

    add default cdk  project

---
+++ test/armada-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ArmadaInfrastructure from '../lib/armada-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/armada-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ArmadaInfrastructure.ArmadaInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
