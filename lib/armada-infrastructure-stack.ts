323af0f9793970b0ee9dea4fa5b89885d50d70a1
Sergio Pichardo <10542280+sergiopichardo@users.noreply.github.com>
2022-10-29 16:47:35 -0500

    add dynamic support code with test task definition

--- lib/armada-infrastructure-stack.ts
+++ lib/armada-infrastructure-stack.ts
@@ -1,14 +1,10 @@
-// import path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-// import * as iam from 'aws-cdk-lib/aws-iam';
-// import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
-// import * as ecrdeploy from 'cdk-ecr-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 
 export class ArmadaInfrastructureStack extends cdk.Stack {
@@ -35,75 +31,88 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
     /****************************************************************
      * Security Groups
     ****************************************************************/
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    // Application Load Balancer Security Groups 
+    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALB-Security-Group', { 
       vpc,
-      description: 'Security Group for Armada Instance',
-      allowAllIpv6Outbound: true,
-      allowAllOutbound: true,
-      securityGroupName: 'Security Group for Armada App',
+      description: "ALB Security Group",
     });
 
-    // Allow SSH
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      'SSH Access'
-    );
-
-    // Allow HTTP
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
+    albSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(), 
+      ec2.Port.tcp(80), 
       'HTTP Access'
     );
 
-    // Allow HTTPS
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      'HTTPS Access'
+    albSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv6(), 
+      ec2.Port.tcp(80), 
+      'HTTP Access'
     );
 
-    // With dynamic port mapping we won't have to
-    // hard code the host container port
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(8080),
-      'Allow Container Access'
+
+    // Launch Template Security Groups
+    const ltSecurityGroup = new ec2.SecurityGroup(this, "Security-Group-for-Launch-Template", {
+      vpc: vpc, 
+      allowAllOutbound: true,
+      description: "Security Group for Launch Template ECS Instances"
+    })
+
+    ltSecurityGroup.connections.allowFrom(
+      new ec2.Connections({
+        securityGroups: [albSecurityGroup],
+      }),
+      ec2.Port.allTraffic(), 
+      "Allow all traffic on all ports coming from Application Load Balancer"
     );
 
-    // Add default capacity to the cluster.
-    const ec2InstanceType = ec2.InstanceType.of(
-      ec2.InstanceClass.T2,
-      ec2.InstanceSize.MICRO,
-    );
+
+    /****************************************************************
+     * Launch Template 
+    ****************************************************************/
+    const launchTemplate = new ec2.LaunchTemplate(this, 'ASG-Launch-Template', {
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2, 
+        ec2.InstanceSize.SMALL
+      ),
+      securityGroup: ltSecurityGroup, 
+      userData: ec2.UserData.forLinux(),
+      role: new iam.Role(this, 'ec2AccessRole', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+      })
+    });
+
+
 
     /****************************************************************
      * Auto Scaling Group 
     ****************************************************************/
     // Explicitly add customized capacity through ASG
-    const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'ASG', {
-      vpc,
-      instanceType: ec2InstanceType,
-      machineImage: ecs.EcsOptimizedImage.amazonLinux(),
-      desiredCapacity: 1, 
+    const asg = new autoscaling.AutoScalingGroup(this, 'Auto-Scaling-Group', {
+      vpc, 
+      launchTemplate,
       minCapacity: 1, 
-      maxCapacity: 3,
-      securityGroup: securityGroup
+      desiredCapacity: 1,
+      maxCapacity: 10
     })
 
-    // ECS Capacity Provider
-    // A capacity provider is an abstraction that uses an ASG underneath
-    // It's a way to make your ECS cluster "ASG-aware". Think ASG adapter for ECS
-    const capacityProvider = new ecs.AsgCapacityProvider(this, 'AsgCapacityProvider', {
-      autoScalingGroup,
+    asg.scaleOnCpuUtilization('cpu-util-scaling', {
+      targetUtilizationPercent: 50
     });
-    
     
     
     /****************************************************************
      * Elastic Container Service
     ****************************************************************/
+    // ECS Capacity Provider
+    // A capacity provider is an abstraction that uses an ASG underneath
+    // It's a way to make your ECS cluster "ASG-aware". Think ASG adapter for ECS
+    // ECS capacity provider 
+    const capacityProvider = new ecs.AsgCapacityProvider(this, 'ASG-Capacity-Provider', {
+      capacityProviderName: "ASG-Capacity-Provider", 
+      autoScalingGroup: asg
+    });
+
     const cluster = new ecs.Cluster(this, 'ECS-Cluster', { 
       clusterName: 'ECS-Cluster', 
       vpc: vpc,
@@ -114,61 +123,58 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
     cluster.addAsgCapacityProvider(capacityProvider)
   
 
-    // register default task definitions HERE (e.g. JS, Ruby, Go)
-
 
     /****************************************************************
      * Application Load Balancer  
-    ****************************************************************/
-    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
-      vpc,
-      internetFacing: true
-    }); 
+    ****************************************************************/ 
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'Load-Balancer', {
+      vpc: vpc,
+      internetFacing: true,
+      securityGroup: albSecurityGroup,
+    });
 
-    // // NOTE: eventually we want to use port 443 and HTTPS
-    const lbListener = loadBalancer.addListener('LBListener', {
-      port: 80, 
+    // TODO: eventually we want to use port 443 and HTTPS
+    const listener = alb.addListener('ALB-Listener', {
+      port: 80,   // listens for requests on port 80
       open: true, // Allow CDK to automatically create security 
-                 // group rule to allow traffic on port 80
-      protocol: elbv2.ApplicationProtocol.HTTP,
+                  // group rule to allow traffic on port 80
+      protocol: elbv2.ApplicationProtocol.HTTP
     }); 
   
     // When you add an autoscaling group as the target 
     // CDK automatically puts the instances associated with that ASG into a target grouop
-    lbListener.addTargets('LoadBalancerListener', {
-      targets: [autoScalingGroup], 
-      healthCheck: { 
+    listener.addTargets('Target-Group-ASG', {
+      port: 80,
+      targets: [asg],
+      healthCheck: { // if problems comment out `healthcheck`
         enabled: true,
-        healthyHttpCodes: '200-299',
+        healthyHttpCodes: '200-299', 
         path: '/'
       }
-    });
+    })
     
-    lbListener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+    // Create CFN Output 
+    new cdk.CfnOutput(this, 'ALB-DNS-name', {
+      value: alb.loadBalancerDnsName,
+    });
 
 
     /****************************************************************
-     * Task Definition + Service (FOR TESTING ONLY)
+     * Task Definition (FOR TESTING ONLY, remove later)
     ****************************************************************/
-    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'Health-Check-Service-Task-Definition', {
-      networkMode: ecs.NetworkMode.BRIDGE,
-    }); 
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'Task-Definition');
 
-    taskDefinition.addContainer('HealthCheckContainer', {
-      image: ecs.ContainerImage.fromRegistry("amazon/health-check-service"), 
-      memoryLimitMiB: 1024,
+    taskDefinition.addContainer("nginxdemos-hello", {
+      image: ecs.ContainerImage.fromRegistry("nginxdemos/hello"),
+      memoryLimitMiB: 512,
       portMappings: [
-        { hostPort: 0, containerPort: 5000 }
+        {
+          hostPort: 0,
+          containerPort: 80,
+          protocol: ecs.Protocol.TCP,
+        }
       ]
-    });
-  
-    // Instantiate an ECS Service 
-    const ecsService = new ecs.Ec2Service(this, 'health-check-service', {
-      cluster, 
-      taskDefinition
-    });    
-
-    
+    }); 
   }
 
 
