ee3350a6dc90ffe00500e3ada81c8fad4ae963b1
Joey <josephdguillaume@gmail.com>
2022-10-31 13:30:29 -0400

    Added EFS, EFS Security Group, EFS Access Point, and AWS Lambda.
    Co-authored-by: Natalie Martos <natalie.martos@gmail.com>

--- lib/armada-infrastructure-stack.ts
+++ lib/armada-infrastructure-stack.ts
@@ -1,19 +1,21 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as efs from 'aws-cdk-lib/aws-efs';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
-
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class ArmadaInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     /****************************************************************
-    * Virtual Private Cloud (VPC)
-    ****************************************************************/
+     * Virtual Private Cloud (VPC)
+     ****************************************************************/
     const vpc = new ec2.Vpc(this, 'VPC', {
       cidr: '10.0.0.0/16',
       maxAzs: 3,
@@ -27,161 +29,234 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
       natGateways: 0,
     });
 
-
     /****************************************************************
      * Security Groups
-    ****************************************************************/
-    // Application Load Balancer Security Groups 
-    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALB-Security-Group', { 
+     ****************************************************************/
+    // Application Load Balancer Security Groups
+    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALB-Security-Group', {
       vpc,
-      description: "ALB Security Group",
+      description: 'ALB Security Group',
     });
 
     albSecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(), 
-      ec2.Port.tcp(80), 
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
       'HTTP Access'
     );
 
     albSecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv6(), 
-      ec2.Port.tcp(80), 
+      ec2.Peer.anyIpv6(),
+      ec2.Port.tcp(80),
       'HTTP Access'
     );
 
-
     // Launch Template Security Groups
-    const ltSecurityGroup = new ec2.SecurityGroup(this, "Launch-Template-Security-Group", {
-      vpc: vpc, 
-      allowAllOutbound: true,
-      description: "Security Group for Launch Template ECS Instances"
-    })
+    const ltSecurityGroup = new ec2.SecurityGroup(
+      this,
+      'Launch-Template-Security-Group',
+      {
+        vpc,
+        allowAllOutbound: true,
+        description: 'Security Group for Launch Template ECS Instances',
+      }
+    );
 
     ltSecurityGroup.connections.allowFrom(
       new ec2.Connections({
         securityGroups: [albSecurityGroup],
       }),
-      ec2.Port.allTraffic(), 
-      "Allow all traffic on all ports coming from Application Load Balancer"
+      ec2.Port.allTraffic(),
+      'Allow all traffic on all ports coming from Application Load Balancer'
     );
 
+    // Security Group for Storage
+    const ArmadaStorageSecurity = new ec2.SecurityGroup(
+      this,
+      'EFSSecurityGroup',
+      {
+        vpc,
+        description: 'Security Group for the Elastic File System',
+        securityGroupName: 'Security Group for Armada Permanent Storage',
+      }
+    );
 
     /****************************************************************
-     * Launch Template 
-    ****************************************************************/
+     * Launch Template
+     ****************************************************************/
     const launchTemplate = new ec2.LaunchTemplate(this, 'ASG-Launch-Template', {
       machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
       instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2, 
+        ec2.InstanceClass.T2,
         ec2.InstanceSize.SMALL
       ),
-      securityGroup: ltSecurityGroup, 
+      securityGroup: ltSecurityGroup,
       userData: ec2.UserData.forLinux(),
       // grant ec2 instances communication access to ECS cluster
       role: new iam.Role(this, 'ec2AccessRole', {
-        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-      })
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      }),
     });
 
-
-
     /****************************************************************
-     * Auto Scaling Group 
-    ****************************************************************/
+     * Auto Scaling Group
+     ****************************************************************/
     // Explicitly add customized capacity through ASG
     const asg = new autoscaling.AutoScalingGroup(this, 'Auto-Scaling-Group', {
-      vpc, 
+      vpc,
       launchTemplate,
-      minCapacity: 1, 
+      minCapacity: 1,
       desiredCapacity: 1,
-      maxCapacity: 10
-    })
+      maxCapacity: 10,
+    });
 
     asg.scaleOnCpuUtilization('cpu-util-scaling', {
-      targetUtilizationPercent: 50
+      targetUtilizationPercent: 50,
     });
-    
-    
+
     /****************************************************************
      * Elastic Container Service
-    ****************************************************************/
+     ****************************************************************/
     // ECS Capacity Provider
     // A capacity provider is an abstraction that uses an ASG underneath
     // It's a way to make your ECS cluster "ASG-aware". Think ASG adapter for ECS
-    // ECS capacity provider 
-    const capacityProvider = new ecs.AsgCapacityProvider(this, 'ASG-Capacity-Provider', {
-      capacityProviderName: "ASG-Capacity-Provider", 
-      // manage cluster scaling using ASG strategy
-      autoScalingGroup: asg
-    });
+    // ECS capacity provider
+    const capacityProvider = new ecs.AsgCapacityProvider(
+      this,
+      'ASG-Capacity-Provider',
+      {
+        capacityProviderName: 'ASG-Capacity-Provider',
+        // manage cluster scaling using ASG strategy
+        autoScalingGroup: asg,
+      }
+    );
 
-    const cluster = new ecs.Cluster(this, 'ECS-Cluster', { 
-      clusterName: 'ECS-Cluster', 
-      vpc: vpc,
+    const cluster = new ecs.Cluster(this, 'ECS-Cluster', {
+      clusterName: 'ECS-Cluster',
+      vpc,
       // collect metrics and logs from your containers
-      containerInsights: true // enabled for CloudWatch logs
+      containerInsights: true, // enabled for CloudWatch logs
     });
-    
-    // Attach Security grouop to ECS cluster
-    cluster.addAsgCapacityProvider(capacityProvider)
-  
 
+    // Attach Security group to ECS cluster
+    cluster.addAsgCapacityProvider(capacityProvider);
 
     /****************************************************************
-     * Application Load Balancer  
-    ****************************************************************/ 
+     * Application Load Balancer
+     ****************************************************************/
     const alb = new elbv2.ApplicationLoadBalancer(this, 'Load-Balancer', {
-      vpc: vpc,
+      vpc,
       internetFacing: true,
       securityGroup: albSecurityGroup,
     });
 
     // TODO: eventually we want to use port 443 and HTTPS
     const listener = alb.addListener('ALB-Listener', {
-      port: 80,   // listens for requests on port 80
-      open: true, // Allow CDK to automatically create security 
-                  // group rule to allow traffic on port 80
-      protocol: elbv2.ApplicationProtocol.HTTP
-    }); 
-  
-    // When you add an autoscaling group as the target 
-    // CDK automatically puts the instances associated with that ASG into a target grouop
+      port: 80, // listens for requests on port 80
+      open: true, // Allow CDK to automatically create security
+      // group rule to allow traffic on port 80
+      protocol: elbv2.ApplicationProtocol.HTTP,
+    });
+
+    // When you add an autoscaling group as the target
+    // CDK automatically puts the instances associated with that ASG into a target group
     listener.addTargets('Target-Group-ASG', {
       port: 80,
-      // send traffic to automatically created Target Group 
+      // send traffic to automatically created Target Group
       // that contains any instance created by Auto Scaling Group
-      targets: [asg], 
-      healthCheck: { 
+      targets: [asg],
+      healthCheck: {
         enabled: true,
-        healthyHttpCodes: '200-299', 
-        path: '/'
-      }
-    })
-    
-    // Create CFN Output 
+        healthyHttpCodes: '200-299',
+        path: '/',
+      },
+    });
+
+    // Create CFN Output
     new cdk.CfnOutput(this, 'ALB-DNS-name', {
       value: alb.loadBalancerDnsName,
     });
 
+    /****************************************************************
+     * Elastic File System
+     ****************************************************************/
+
+    const ArmadaPermanentStorage = new efs.FileSystem(
+      this,
+      'ArmadaFileSystem',
+      {
+        vpc,
+        securityGroup: ArmadaStorageSecurity,
+        lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
+        performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+        outOfInfrequentAccessPolicy:
+          efs.OutOfInfrequentAccessPolicy.AFTER_1_ACCESS,
+        enableAutomaticBackups: true,
+        fileSystemName: 'ArmadaPermanentStorage',
+      }
+    );
+
+    ArmadaPermanentStorage.connections.allowFrom(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(2049),
+      'Allow Network File Storage'
+    );
+
+    // Add an Access Point for the Lambda to use.
+    const lambdaEFSAccessPoint = new efs.AccessPoint(
+      this,
+      'ArmadaLambdaAccess',
+      {
+        fileSystem: ArmadaPermanentStorage,
+        createAcl: {
+          ownerUid: '0',
+          ownerGid: '0',
+          permissions: '0777',
+        },
+        posixUser: {
+          uid: '0',
+          gid: '0',
+        },
+      }
+    );
 
     /****************************************************************
-     * Task Definition (FOR TESTING ONLY, remove later)
-    ****************************************************************/
-    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'Task-Definition');
+     * Lambda
+     ****************************************************************/
 
-    taskDefinition.addContainer("nginxdemos-hello", {
-      image: ecs.ContainerImage.fromRegistry("nginxdemos/hello"),
-      memoryLimitMiB: 512,
-      portMappings: [
+    const lambdaAccessRole = new iam.Role(this, 'LambdaAccessRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      description:
+        'Allows the AWS Lambda access to the VPC and provides write functionality to EFS.',
+      managedPolicies: [
         {
-          hostPort: 0,
-          containerPort: 80,
-          protocol: ecs.Protocol.TCP,
-        }
-      ]
-    }); 
+          managedPolicyArn:
+            'arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole',
+        },
+
+        {
+          managedPolicyArn:
+            'arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess',
+        },
+      ],
+    });
+
+    const storageLambda = new lambda.Function(this, 'createEFSFolders', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'createEFSFolders.handler',
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, '../lambdas/createEFSFolders')
+      ),
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
+      securityGroups: [ArmadaStorageSecurity],
+      role: lambdaAccessRole,
+      vpc,
+      filesystem: lambda.FileSystem.fromEfsAccessPoint(
+        lambdaEFSAccessPoint,
+        '/mnt/efs'
+      ),
+      allowPublicSubnet: true,
+      functionName: 'createEFSFolders',
+    });
   }
-
-
 }
-
