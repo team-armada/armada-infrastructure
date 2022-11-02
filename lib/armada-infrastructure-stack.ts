5567acdc61cfb4fa7572ddd1082f9bd8c36d3af3
Sergio Pichardo <10542280+sergiopichardo@users.noreply.github.com>
2022-11-02 14:47:48 -0500

    set cognito removal policy to DESTROY

--- lib/armada-infrastructure-stack.ts
+++ lib/armada-infrastructure-stack.ts
@@ -15,249 +15,249 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    /****************************************************************
-     * Virtual Private Cloud (VPC)
-     ****************************************************************/
-    const vpc = new ec2.Vpc(this, 'VPC', {
-      cidr: '10.0.0.0/16',
-      maxAzs: 3,
-      subnetConfiguration: [
-        {
-          name: 'publicSubnet',
-          subnetType: ec2.SubnetType.PUBLIC,
-          cidrMask: 24,
-        },
-      ],
-      natGateways: 0,
-    });
+    // /****************************************************************
+    //  * Virtual Private Cloud (VPC)
+    //  ****************************************************************/
+    // const vpc = new ec2.Vpc(this, 'VPC', {
+    //   cidr: '10.0.0.0/16',
+    //   maxAzs: 3,
+    //   subnetConfiguration: [
+    //     {
+    //       name: 'publicSubnet',
+    //       subnetType: ec2.SubnetType.PUBLIC,
+    //       cidrMask: 24,
+    //     },
+    //   ],
+    //   natGateways: 0,
+    // });
 
-    /****************************************************************
-     * Security Groups
-     ****************************************************************/
-    // Application Load Balancer Security Groups
-    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALB-Security-Group', {
-      vpc,
-      description: 'ALB Security Group',
-    });
+    // /****************************************************************
+    //  * Security Groups
+    //  ****************************************************************/
+    // // Application Load Balancer Security Groups
+    // const albSecurityGroup = new ec2.SecurityGroup(this, 'ALB-Security-Group', {
+    //   vpc,
+    //   description: 'ALB Security Group',
+    // });
 
-    albSecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'HTTP Access'
-    );
+    // albSecurityGroup.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(80),
+    //   'HTTP Access'
+    // );
 
-    albSecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv6(),
-      ec2.Port.tcp(80),
-      'HTTP Access'
-    );
+    // albSecurityGroup.addIngressRule(
+    //   ec2.Peer.anyIpv6(),
+    //   ec2.Port.tcp(80),
+    //   'HTTP Access'
+    // );
 
-    // Launch Template Security Groups
-    const ltSecurityGroup = new ec2.SecurityGroup(
-      this,
-      'Launch-Template-Security-Group',
-      {
-        vpc,
-        allowAllOutbound: true,
-        description: 'Security Group for Launch Template ECS Instances',
-      }
-    );
+    // // Launch Template Security Groups
+    // const ltSecurityGroup = new ec2.SecurityGroup(
+    //   this,
+    //   'Launch-Template-Security-Group',
+    //   {
+    //     vpc,
+    //     allowAllOutbound: true,
+    //     description: 'Security Group for Launch Template ECS Instances',
+    //   }
+    // );
 
-    ltSecurityGroup.connections.allowFrom(
-      new ec2.Connections({
-        securityGroups: [albSecurityGroup],
-      }),
-      ec2.Port.allTraffic(),
-      'Allow all traffic on all ports coming from Application Load Balancer'
-    );
+    // ltSecurityGroup.connections.allowFrom(
+    //   new ec2.Connections({
+    //     securityGroups: [albSecurityGroup],
+    //   }),
+    //   ec2.Port.allTraffic(),
+    //   'Allow all traffic on all ports coming from Application Load Balancer'
+    // );
 
-    // Security Group for Storage
-    const ArmadaStorageSecurity = new ec2.SecurityGroup(
-      this,
-      'EFSSecurityGroup',
-      {
-        vpc,
-        description: 'Security Group for the Elastic File System',
-        securityGroupName: 'Security Group for Armada Permanent Storage',
-      }
-    );
+    // // Security Group for Storage
+    // const ArmadaStorageSecurity = new ec2.SecurityGroup(
+    //   this,
+    //   'EFSSecurityGroup',
+    //   {
+    //     vpc,
+    //     description: 'Security Group for the Elastic File System',
+    //     securityGroupName: 'Security Group for Armada Permanent Storage',
+    //   }
+    // );
 
-    /****************************************************************
-     * Launch Template
-     ****************************************************************/
-    const launchTemplate = new ec2.LaunchTemplate(this, 'ASG-Launch-Template', {
-      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.SMALL
-      ),
-      securityGroup: ltSecurityGroup,
-      userData: ec2.UserData.forLinux(),
-      // grant ec2 instances communication access to ECS cluster
-      role: new iam.Role(this, 'ec2AccessRole', {
-        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      }),
-    });
+    // /****************************************************************
+    //  * Launch Template
+    //  ****************************************************************/
+    // const launchTemplate = new ec2.LaunchTemplate(this, 'ASG-Launch-Template', {
+    //   machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
+    //   instanceType: ec2.InstanceType.of(
+    //     ec2.InstanceClass.T2,
+    //     ec2.InstanceSize.SMALL
+    //   ),
+    //   securityGroup: ltSecurityGroup,
+    //   userData: ec2.UserData.forLinux(),
+    //   // grant ec2 instances communication access to ECS cluster
+    //   role: new iam.Role(this, 'ec2AccessRole', {
+    //     assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    //   }),
+    // });
 
-    /****************************************************************
-     * Auto Scaling Group
-     ****************************************************************/
-    // Explicitly add customized capacity through ASG
-    const asg = new autoscaling.AutoScalingGroup(this, 'Auto-Scaling-Group', {
-      vpc,
-      launchTemplate,
-      minCapacity: 1,
-      desiredCapacity: 1,
-      maxCapacity: 10,
-    });
+    // /****************************************************************
+    //  * Auto Scaling Group
+    //  ****************************************************************/
+    // // Explicitly add customized capacity through ASG
+    // const asg = new autoscaling.AutoScalingGroup(this, 'Auto-Scaling-Group', {
+    //   vpc,
+    //   launchTemplate,
+    //   minCapacity: 1,
+    //   desiredCapacity: 1,
+    //   maxCapacity: 10,
+    // });
 
-    asg.scaleOnCpuUtilization('cpu-util-scaling', {
-      targetUtilizationPercent: 50,
-    });
+    // asg.scaleOnCpuUtilization('cpu-util-scaling', {
+    //   targetUtilizationPercent: 50,
+    // });
 
-    /****************************************************************
-     * Elastic Container Service
-     ****************************************************************/
-    // ECS Capacity Provider
-    // A capacity provider is an abstraction that uses an ASG underneath
-    // It's a way to make your ECS cluster "ASG-aware". Think ASG adapter for ECS
-    // ECS capacity provider
-    const capacityProvider = new ecs.AsgCapacityProvider(
-      this,
-      'ASG-Capacity-Provider',
-      {
-        capacityProviderName: 'ASG-Capacity-Provider',
-        // manage cluster scaling using ASG strategy
-        autoScalingGroup: asg,
-      }
-    );
+    // /****************************************************************
+    //  * Elastic Container Service
+    //  ****************************************************************/
+    // // ECS Capacity Provider
+    // // A capacity provider is an abstraction that uses an ASG underneath
+    // // It's a way to make your ECS cluster "ASG-aware". Think ASG adapter for ECS
+    // // ECS capacity provider
+    // const capacityProvider = new ecs.AsgCapacityProvider(
+    //   this,
+    //   'ASG-Capacity-Provider',
+    //   {
+    //     capacityProviderName: 'ASG-Capacity-Provider',
+    //     // manage cluster scaling using ASG strategy
+    //     autoScalingGroup: asg,
+    //   }
+    // );
 
-    const cluster = new ecs.Cluster(this, 'ECS-Cluster', {
-      clusterName: 'ECS-Cluster',
-      vpc,
-      // collect metrics and logs from your containers
-      containerInsights: true, // enabled for CloudWatch logs
-    });
+    // const cluster = new ecs.Cluster(this, 'ECS-Cluster', {
+    //   clusterName: 'ECS-Cluster',
+    //   vpc,
+    //   // collect metrics and logs from your containers
+    //   containerInsights: true, // enabled for CloudWatch logs
+    // });
 
-    // Attach Security group to ECS cluster
-    cluster.addAsgCapacityProvider(capacityProvider);
+    // // Attach Security group to ECS cluster
+    // cluster.addAsgCapacityProvider(capacityProvider);
 
-    /****************************************************************
-     * Application Load Balancer
-     ****************************************************************/
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'Load-Balancer', {
-      vpc,
-      internetFacing: true,
-      securityGroup: albSecurityGroup,
-    });
+    // /****************************************************************
+    //  * Application Load Balancer
+    //  ****************************************************************/
+    // const alb = new elbv2.ApplicationLoadBalancer(this, 'Load-Balancer', {
+    //   vpc,
+    //   internetFacing: true,
+    //   securityGroup: albSecurityGroup,
+    // });
 
-    // TODO: eventually we want to use port 443 and HTTPS
-    const listener = alb.addListener('ALB-Listener', {
-      port: 80, // listens for requests on port 80
-      open: true, // Allow CDK to automatically create security
-      // group rule to allow traffic on port 80
-      protocol: elbv2.ApplicationProtocol.HTTP,
-    });
+    // // TODO: eventually we want to use port 443 and HTTPS
+    // const listener = alb.addListener('ALB-Listener', {
+    //   port: 80, // listens for requests on port 80
+    //   open: true, // Allow CDK to automatically create security
+    //   // group rule to allow traffic on port 80
+    //   protocol: elbv2.ApplicationProtocol.HTTP,
+    // });
 
-    // When you add an autoscaling group as the target
-    // CDK automatically puts the instances associated with that ASG into a target group
-    listener.addTargets('Target-Group-ASG', {
-      port: 80,
-      // send traffic to automatically created Target Group
-      // that contains any instance created by Auto Scaling Group
-      targets: [asg],
-      healthCheck: {
-        enabled: true,
-        healthyHttpCodes: '200-299',
-        path: '/',
-      },
-    });
+    // // When you add an autoscaling group as the target
+    // // CDK automatically puts the instances associated with that ASG into a target group
+    // listener.addTargets('Target-Group-ASG', {
+    //   port: 80,
+    //   // send traffic to automatically created Target Group
+    //   // that contains any instance created by Auto Scaling Group
+    //   targets: [asg],
+    //   healthCheck: {
+    //     enabled: true,
+    //     healthyHttpCodes: '200-299',
+    //     path: '/',
+    //   },
+    // });
 
-    // Create CFN Output
-    new cdk.CfnOutput(this, 'ALB-DNS-name', {
-      value: alb.loadBalancerDnsName,
-    });
+    // // Create CFN Output
+    // new cdk.CfnOutput(this, 'ALB-DNS-name', {
+    //   value: alb.loadBalancerDnsName,
+    // });
 
-    /****************************************************************
-     * Elastic File System
-     ****************************************************************/
+    // /****************************************************************
+    //  * Elastic File System
+    //  ****************************************************************/
 
-    const ArmadaPermanentStorage = new efs.FileSystem(
-      this,
-      'ArmadaFileSystem',
-      {
-        vpc,
-        securityGroup: ArmadaStorageSecurity,
-        lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
-        performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
-        outOfInfrequentAccessPolicy:
-          efs.OutOfInfrequentAccessPolicy.AFTER_1_ACCESS,
-        enableAutomaticBackups: true,
-        fileSystemName: 'ArmadaPermanentStorage',
-      }
-    );
+    // const ArmadaPermanentStorage = new efs.FileSystem(
+    //   this,
+    //   'ArmadaFileSystem',
+    //   {
+    //     vpc,
+    //     securityGroup: ArmadaStorageSecurity,
+    //     lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
+    //     performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+    //     outOfInfrequentAccessPolicy:
+    //       efs.OutOfInfrequentAccessPolicy.AFTER_1_ACCESS,
+    //     enableAutomaticBackups: true,
+    //     fileSystemName: 'ArmadaPermanentStorage',
+    //   }
+    // );
 
-    ArmadaPermanentStorage.connections.allowFrom(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(2049),
-      'Allow Network File Storage'
-    );
+    // ArmadaPermanentStorage.connections.allowFrom(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(2049),
+    //   'Allow Network File Storage'
+    // );
 
-    // Add an Access Point for the Lambda to use.
-    const lambdaEFSAccessPoint = new efs.AccessPoint(
-      this,
-      'ArmadaLambdaAccess',
-      {
-        fileSystem: ArmadaPermanentStorage,
-        createAcl: {
-          ownerUid: '0',
-          ownerGid: '0',
-          permissions: '0777',
-        },
-        posixUser: {
-          uid: '0',
-          gid: '0',
-        },
-      }
-    );
+    // // Add an Access Point for the Lambda to use.
+    // const lambdaEFSAccessPoint = new efs.AccessPoint(
+    //   this,
+    //   'ArmadaLambdaAccess',
+    //   {
+    //     fileSystem: ArmadaPermanentStorage,
+    //     createAcl: {
+    //       ownerUid: '0',
+    //       ownerGid: '0',
+    //       permissions: '0777',
+    //     },
+    //     posixUser: {
+    //       uid: '0',
+    //       gid: '0',
+    //     },
+    //   }
+    // );
 
-    /****************************************************************
-     * Lambda
-     ****************************************************************/
+    // /****************************************************************
+    //  * Lambda
+    //  ****************************************************************/
 
-    const lambdaAccessRole = new iam.Role(this, 'LambdaAccessRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      description:
-        'Allows the AWS Lambda access to the VPC and provides write functionality to EFS.',
-      managedPolicies: [
-        {
-          managedPolicyArn:
-            'arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole',
-        },
+    // const lambdaAccessRole = new iam.Role(this, 'LambdaAccessRole', {
+    //   assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    //   description:
+    //     'Allows the AWS Lambda access to the VPC and provides write functionality to EFS.',
+    //   managedPolicies: [
+    //     {
+    //       managedPolicyArn:
+    //         'arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole',
+    //     },
 
-        {
-          managedPolicyArn:
-            'arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess',
-        },
-      ],
-    });
+    //     {
+    //       managedPolicyArn:
+    //         'arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess',
+    //     },
+    //   ],
+    // });
 
-    const storageLambda = new lambda.Function(this, 'createEFSFolders', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: 'createEFSFolders.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(5),
-      securityGroups: [ArmadaStorageSecurity],
-      role: lambdaAccessRole,
-      vpc,
-      filesystem: lambda.FileSystem.fromEfsAccessPoint(
-        lambdaEFSAccessPoint,
-        '/mnt/efs'
-      ),
-      allowPublicSubnet: true,
-      functionName: 'createEFSFolders',
-    });
+    // const storageLambda = new lambda.Function(this, 'createEFSFolders', {
+    //   runtime: lambda.Runtime.NODEJS_16_X,
+    //   handler: 'createEFSFolders.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
+    //   memorySize: 128,
+    //   timeout: cdk.Duration.seconds(5),
+    //   securityGroups: [ArmadaStorageSecurity],
+    //   role: lambdaAccessRole,
+    //   vpc,
+    //   filesystem: lambda.FileSystem.fromEfsAccessPoint(
+    //     lambdaEFSAccessPoint,
+    //     '/mnt/efs'
+    //   ),
+    //   allowPublicSubnet: true,
+    //   functionName: 'createEFSFolders',
+    // });
 
 
     /****************************************************************
@@ -265,7 +265,7 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
     ****************************************************************/
     // ATTENTION: Cognito user pools are immutable
     // once a user pool has been created it cannot be changed.
-    const userPool = new cognito.UserPool(this, 'Cognito-User-Pool', {
+    const cognitoUserPool = new cognito.UserPool(this, 'Cognito-User-Pool', {
       userPoolName: 'Cognito-User-Pool', 
       signInCaseSensitive: false, 
       // users are allowed to sign up
@@ -343,8 +343,8 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
       // how users can recover their account if they forget their password 
       accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
       // whether the user pool should be retained in the account after the 
-      // stack is deleted. 
-      removalPolicy: cdk.RemovalPolicy.RETAIN, 
+      // stack is deleted. set `cdk.RemovalPolicy.` to `RETAIN` when launching to production 
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
 
       /* User verification 
         - When a user signs up, email and SMS messages are used to verify their 
@@ -363,7 +363,23 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
       },
 
       email: cognito.UserPoolEmail.withCognito("support@releasethefleet.com")
-    }); 
+    });
+    
+    // Cognito App Client 
+    const client = cognitoUserPool.addClient('Cognito-App-Client'); 
+
+    // Outputs
+    new cdk.CfnOutput(this, 'userPoolId', {
+      value: cognitoUserPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, 'userPoolClientId', {
+      value: client.userPoolClientId,
+    });
+    
+    
+
+
 
     // --------------------------------------------------------------------
     // NOTE: Front-end testing 
