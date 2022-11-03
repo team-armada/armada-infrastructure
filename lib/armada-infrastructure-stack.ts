88b5c1081af404823e0e9ef431f3e08d01cb3ee4
Dean Elizardo <del@delizardo.com>
2022-11-03 15:45:05 -0500

    merged in credentialling changes for rds
    co-author: Sergio Pichardo sergio.j.pichardo@users.noreply.github.com

--- lib/armada-infrastructure-stack.ts
+++ lib/armada-infrastructure-stack.ts
@@ -9,7 +9,7 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as rds from 'aws-cdk-lib/aws-rds';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class ArmadaInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -289,7 +289,30 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
      * Database (RDS)
      ****************************************************************/
 
-    // const databaseSecret = new secretsmanager.Secret(this, "SOME_ENV_SECRET_HERE");
+    // first, lets generate a secret to be used as credentials for our database
+    const databaseCredentialsSecret = new secretsManager.Secret(this, `database-DBCredentialsSecret`, {
+      secretName: `database-credentials`,
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: 'postgres',
+        }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'password'
+      }
+    });
+
+    // lets output a few properties to help use find the credentials
+    new cdk.CfnOutput(this, 'Secret Name', { value: databaseCredentialsSecret.secretName });
+    new cdk.CfnOutput(this, 'Secret ARN', { value: databaseCredentialsSecret.secretArn });
+    new cdk.CfnOutput(this, 'Secret Full ARN', { value: databaseCredentialsSecret.secretFullArn || '' });
+
+    // next, create a new string parameter to be used
+    new ssm.StringParameter(this, 'DBCredentialsArn', {
+      parameterName: `database-credentials-arn`,
+      stringValue: databaseCredentialsSecret.secretArn,
+    });
+
     const engine = rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13_7 });
 
     const dbInstance = new rds.DatabaseInstance(this, 'Postgres-Database', {
@@ -302,8 +325,7 @@ export class ArmadaInfrastructureStack extends cdk.Stack {
         ec2.InstanceClass.BURSTABLE3,
         ec2.InstanceSize.MICRO,
       ),
-      // credentials: rds.Credentials.fromGeneratedSecret('postgres'),
-      // credentials: rds.Credentials.fromSecret(databaseSecret),
+
       multiAz: false,
       allocatedStorage: 100,
       maxAllocatedStorage: 105,
