2ba1a5c9734a147b3f8ee37c1f873562f04a4a4d
Sergio Pichardo <10542280+sergiopichardo@users.noreply.github.com>
2022-11-05 20:27:16 -0500

    add ASG, ECS and ALB stacks

--- lib/armada-root-stack.ts
+++ lib/armada-root-stack.ts
@@ -4,7 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 // Nested Stack
 import { VPCStack } from './vpc-nested-stack';
 import { EFSStack } from './efs-stack';
-
+import { CognitoStack } from './cognito-stack';
+import { ASGStack } from './asg-stack';
+import { ALBStack } from "./load-balancer-stack";
+import { ECSStack } from "./ecs-stack";
 
 
 export class ArmadaRootStack extends cdk.Stack {
@@ -16,6 +19,7 @@ export class ArmadaRootStack extends cdk.Stack {
       description: "VPC and Security Groups stack"
     }); 
 
+
     // File System 
     const efs = new EFSStack(this, "EFS-Stack", {
       vpc: infra.vpc,
@@ -24,7 +28,41 @@ export class ArmadaRootStack extends cdk.Stack {
 
     efs.addDependency(infra); 
 
+
+    // Auto Scaling Group 
+    const asg = new ASGStack(this, "ASG-Stack", {
+      vpc: infra.vpc,
+      launchTemplateSecurityGroup: infra.launchTemplateSecurityGroup
+    })
+
     // Load Balancer 
+    const loadBalancer = new ALBStack(this, "ALB-Stack", {
+      vpc: infra.vpc,
+      albSecurityGroup: infra.albSecurityGroup,
+      autoScalingGroup: asg.autoScalingGroup
+    })
+
+    loadBalancer.addDependency(asg); 
+
+
+    // ECS Cluster 
+    const ecs = new ECSStack(this, 'ECS-Stack', {
+      vpc: infra.vpc, 
+      autoScalingGroup: asg.autoScalingGroup
+    })
+
+    ecs.addDependency(asg); 
+
+
+    // Cognito 
+    const cognito = new CognitoStack(this, "Cognito-Stack", {}); 
+
+
+    // RDS Database 
+
+
+    // SPA Stack 
+
     
 
   }
---
+++ lib/asg-stack.ts
@@ -0,0 +1,43 @@
+import { Construct } from 'constructs'; 
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'; 
+import * as ecs from 'aws-cdk-lib/aws-ecs'; 
+import * as iam from 'aws-cdk-lib/aws-iam'; 
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling'; 
+
+interface ASGStackProps extends cdk.NestedStackProps {
+  vpc: ec2.Vpc; 
+  launchTemplateSecurityGroup: ec2.SecurityGroup; 
+}
+
+
+export class ASGStack extends cdk.NestedStack {
+  public autoScalingGroup: autoscaling.AutoScalingGroup; 
+  public launchTemplate: ec2.LaunchTemplate;
+
+  constructor(scope: Construct, id: string, props: ASGStackProps) {
+    super(scope, id, props); 
+
+    this.launchTemplate = new ec2.LaunchTemplate(this, 'ASG-Launch-Template', {
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.SMALL
+      ),
+      securityGroup: props.launchTemplateSecurityGroup,
+      userData: ec2.UserData.forLinux(),
+      // grant ec2 instances communication access to ECS cluster
+      role: new iam.Role(this, 'ec2AccessRole', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      }),
+    });
+
+    this.autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'Auto-Scaling-Group', {
+      vpc: props.vpc, 
+      launchTemplate: this.launchTemplate,
+      minCapacity: 1, 
+      desiredCapacity: 1, 
+      maxCapacity: 10
+    }); 
+  }
+}
