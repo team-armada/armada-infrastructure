d346ead8432c9c5cd3076c3fbf6c0a6bfc81d862
Sergio Pichardo <10542280+sergiopichardo@users.noreply.github.com>
2022-11-14 19:06:54 -0600

    pass props obtained from env vars to ArmadaInfraStack

--- bin/armada-infrastructure.ts
+++ bin/armada-infrastructure.ts
@@ -7,6 +7,9 @@ const app = new cdk.App();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const stack = new ArmadaInfraStack(app, 'ArmadaInfraStack', {
+  adminNodeKeyPairName: process.env.ADMIN_NODE_KEY_PAIR_NAME, // e.g. 'my-us-east-2-key-pair'
+  region: process.env.AWS_DEFAULT_REGION, // e.g. 'us-east-2'
+  availabilityZone: process.env.AWS_AVAILABILITY_ZONE, // e.g. 'us-east-2a'
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKeyId: process.env.AWS_SECRET_ACCESS_KEY,
   env: {
